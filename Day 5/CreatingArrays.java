1182b01aefda5efb0a5ea9a6f86eed678becd42b
skiran2003 <123644615+skiran2003@users.noreply.github.com>
2025-07-20 21:17:43 +0530

    Add files via upload

---
+++ Day 5/CreatingArrays.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class CreatingArrays {
+	public static void main(String[] args) {
+		int [] a = new int[5];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+		System.out.println(a[0]);
+	}
+}
