cc535fe09041fecc76230a9006aff5fd554c949c
skiran2003 <123644615+skiran2003@users.noreply.github.com>
2025-07-21 18:14:27 +0530

    Delete Day 6/OOPS-day1(python)/person.py

--- Day 6/OOPS-day1(python)/person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def greet(self):
-        print(f"Hello {self.name}! Welcome")
-
-p=Person("Sai",22)
-p.greet()
